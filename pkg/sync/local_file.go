e7ffac47f4fd7d4c97f0d739a03ea8606ee8d712
Mael Regnery <mael@mqli.fr>
2021-06-04 23:03:38 +0200

    move sync package and comment

--- pkg/sync/local_file.go
+++ pkg/sync/local_file.go
@@ -2,16 +2,19 @@ package sync
 
 import "github.com/mqllr/kubenv/pkg/k8s"
 
+// LocalFile is the path of a kubeconfig file
 type LocalFile struct {
 	path string
 }
 
+// NewLocalFile creates a LocalFile
 func NewLocalFile(path string) *LocalFile {
 	return &LocalFile{
 		path: path,
 	}
 }
 
+// GetKubeConfig just returns a KubeConfig
 func (local *LocalFile) GetKubeConfig() (*k8s.KubeConfig, error) {
 	return k8s.NewKubeConfigFromFile(local.path)
 }
